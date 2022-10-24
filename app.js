a2a71248d6e426ed18bd7fa8dfe55ac735096679
yessygc <yessgarciacampos@gmail.com>
2022-10-23 23:25:48 -0500

    Manejo de errores e inserción de registros

--- app.js
+++ app.js
@@ -16,3 +16,39 @@ request.onupgradeneeded = event => {
 
 };
 
+//Manejo de errores
+request.onerror = event => {
+    console.log('DB error:', event.target.error );
+};
+
+
+//Insertar datos
+request.onsuccess = event => {
+    let db = event.target.result;
+
+    let heroesData = [
+        { id: '1111', heroe: 'Spiderman', mensaje: 'Aquí su amigo Spiderman' },
+        { id: '2222', heroe: 'Ironman', mensaje: 'Aquí su amigo Ironman' }
+
+    ];
+
+
+    let heroesTransaction = db.transaction('heores', 'readwrite');
+
+    heroesTransactions.onerror = event => {
+        console.log('Error guardado', event.target.error );
+    };
+
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+    for ( let heroe of heroesData ) {
+        heroesStore.add( heroe );
+    }
+
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregado a la base de datos');
+    };
+
+    
+};
+
