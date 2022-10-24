c684b6e64b9540c3a1232f1d11c4f1eaccf46062
yessygc <yessgarciacampos@gmail.com>
2022-10-23 21:58:17 -0500

    Inicios en indexdb

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 
 // indexedDB: Reforzamiento
+let request = window.indexDB.open('mi-database', 1);
 
+//Actualiza cuando se crea o se sube de versión de la BD
+request.onupgradeneeded = event => {
 
+    console.log('Actualización de BD');
 
+    let db = event.terget.result;
+
+    db.createObjectStore('heroes', {
+        keyPath: 'id'
+
+    });
+
+};
 
